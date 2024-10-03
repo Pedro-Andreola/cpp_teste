c0f664a80314373e612a3af7131025b4ce92f9bb
Pedro Andreola <116236339+Pedro-Andreola@users.noreply.github.com>
2024-10-03 18:59:53 -0300

    Create questao2.cpp

---
+++ questao2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int val, ult = 1, penult = 0, temp;
+  cout << "Digite um numero ";
+  cin >> val;
+  do{
+    temp = ult;
+    ult = penult + ult;
+    penult = temp;
+  }while(ult <= val);
+  if(val == penult){
+    cout << "\n O numero faz parte da sequencia de fibonacci";
+  }else{
+    cout << "\n O numero nao faz parte da sequencia de fibonacci";
+  }  
+}

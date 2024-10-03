337f0d697df03e9c841552633ef9dc3e83a2a2c3
Pedro Andreola <116236339+Pedro-Andreola@users.noreply.github.com>
2024-10-03 19:12:21 -0300

    Update questao4.cpp

--- questao4.cpp
+++ questao4.cpp
@@ -4,4 +4,5 @@ int main(){
   float sp = 67836.43, rj = 36678.66, mg = 29229.88, es = 27165.48, outros = 19849.53;
   float total = sp + rj + mg + es + outros;
   cout << "Porcentagens por estado:\n SP:" << (sp/total)*100 << "%\n RJ:" << (rj/total)*100 << " %\n MG:" << (mg/total)*100 << "%\n ES:" << (es/total)*100 << "%\n Outros:" << (outros/total)*100 << " %\n";
+  return 0;
 }
